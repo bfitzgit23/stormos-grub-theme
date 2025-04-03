26f4b68db7dda0e8f9ae5a215c8206eddae272da
ben <dfitz@me.com>
2025-04-03 00:19:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
-THEME_NAME=stormos-xfce 
+THEME_NAME=stormos 
 
 MAX_DELAY=20                                        # max delay for user to enter root password
 
